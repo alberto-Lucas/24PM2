7236e20d00f58e3bd2d4a3f8e99ade517dfc7e91
Lucas Alberto <lucas.alberto@linx.com>
2024-08-12 09:33:26 -0300

    ProjetoBase
    ProjetoBase

---
+++ ProjetoBase/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoBase
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
